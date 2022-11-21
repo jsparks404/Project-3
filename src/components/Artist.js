cab2f42c081e308f8fabd91d483cec38f390ffeb
Josh Sparks <sparksj404@gmail.com>
2022-11-21 17:02:13 -0500

    artist

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -23,7 +23,8 @@ const Artist = () => {
         <div className="artistDiv" key={id}>
             <Covers />
             {data ? <h1 className="artist">{data.artist}</h1> : <h3>Loading...</h3>}
-
+            <Link key={id} to={`/album/${id}`}>
+            <h3 className="albumLink">Album</h3>
             </Link>
             <Link to={'/'}>
                 <p className="homeLink">Return home</p>
